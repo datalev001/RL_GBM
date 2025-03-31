7d5e7fbf52a92bd2cf03d02825d26db804d32698
datalev001 <88938839+datalev001@users.noreply.github.com>
2025-03-31 12:16:59 -0400

    Update RL_GBM_purchase.py

--- code/RL_GBM_purchase.py
+++ code/RL_GBM_purchase.py
@@ -1,4 +1,5 @@
-#######RL + GBM Regression ####################
+#######The RL GBM Regression for predicting Amount Using Using Synthetic Data####################
+# The target of regression models is 'Amount'
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
